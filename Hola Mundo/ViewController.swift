23af7daa0dcf1a75859a35905bb5e0cefdc8f99e
Diego Fernando Diaz Pinilla <diedia@diedia.eltiempo.com.co>
2017-11-22 10:40:03 -0500

    Initial Commit

---
+++ Hola Mundo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Hola Mundo
+//
+//  Created by Diego Fernando Diaz Pinilla on 22/11/17.
+//  Copyright © 2017 NextU. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
